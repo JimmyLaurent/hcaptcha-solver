91a4823689b5735b75c4e968befa218bff88e744
Energon89 <podymov89@gmail.com>
2021-05-21 17:55:21 +0300

    Add getting answers in parallel

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ async function getHSL(req) {
 
 async function getAnswers(request_image, tasks) {
   let answers = new Map();
-  for (const task of tasks) {
+
+  await Promise.all(tasks.map(async (task) => {
     await client.objectLocalization(task.datapoint_uri).then((res) => {
       let [data] = res;
       if (data.localizedObjectAnnotations.find((i) => i.name.toUpperCase() === request_image.toUpperCase() && i.score > 0.5)) {
@@ -43,157 +44,162 @@ async function getAnswers(request_image, tasks) {
         answers[task.task_key] = "false";
       }
     });
-  }
+  }));
 
   return answers;
 }
 
 async function tryToSolve(userAgent, sitekey, host) {
-  // Create headers
-  let headers = {
-    "Authority": "hcaptcha.com",
-    "Accept": "application/json",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Origin": "https://assets.hcaptcha.com",
-    "Sec-Fetch-Site": "same-site",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
-    "User-Agent": userAgent
+  try {
+    // Create headers
+    let headers = {
+      "Authority": "hcaptcha.com",
+      "Accept": "application/json",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Origin": "https://assets.hcaptcha.com",
+      "Sec-Fetch-Site": "same-site",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Dest": "empty",
+      "User-Agent": userAgent
+    };
+    
+    // Check site config
+    let response = await request({
+      method: 'get',
+      headers,
+      json: true,
+      url: `https://hcaptcha.com/checksiteconfig?host=${host}&sitekey=${sitekey}&sc=1&swa=1`
+    });
+
+    let timestamp = Date.now() + rdn(30, 120);
+    
+    // Setup form for getting tasks list 
+    if (response.c === undefined) {
+      form = {
+        sitekey,
+        host,
+        hl: 'en',
+        motionData: {
+          st: timestamp,
+          mm: getMouseMovements(timestamp)
+        },
+      }
+    } else {
+      form = {
+        sitekey,
+        host,
+        hl: 'en',
+        motionData: {
+          st: timestamp,
+          mm: getMouseMovements(timestamp)
+        },
+        n: await getHSL(response.c.req),
+        c: JSON.stringify(response.c)
+      }
+    }
+
+    // Get tasks
+    let getTasks = await request({
+      method: "post",
+      headers,
+      json: true,
+      url: `https://hcaptcha.com/getcaptcha`,
+      form: form
+    });
+
+    if (getTasks.generated_pass_UUID) {
+      return getTasks.generated_pass_UUID;
+    }
+
+    // Find what the captcha is looking for user's to click
+    const requestImageArray = getTasks.requester_question.en.split(" ");
+    let request_image = requestImageArray[requestImageArray.length - 1];
+    if (request_image === "motorbus") {
+      request_image = "bus"
+    } else {
+      request_image = requestImageArray[requestImageArray.length - 1];
+    }
+
+    const key = getTasks.key;
+    if (key.charAt(2) === "_") {
+      return key;
+    }
+
+    const tasks = getTasks.tasklist;
+    const job = getTasks.request_type;
+    timestamp = Date.now() + rdn(30, 120);
+
+    // Get Answers
+    const answers = await getAnswers(request_image, tasks);
+
+    // Renew response
+    response = await request({
+      method: 'get',
+      headers,
+      json: true,
+      url: `https://hcaptcha.com/checksiteconfig?host=${host}&sitekey=${sitekey}&sc=1&swa=1`
+    });
+
+    // Setup data for checking answers
+    if (response.c === undefined) {
+      captchaResponse = {
+        job_mode: job,
+        answers,
+        serverdomain: host,
+        sitekey,
+        motionData: JSON.stringify({
+          st: timestamp,
+          dct: timestamp,
+          mm: getMouseMovements(timestamp),
+        }),
+        n: null,
+        c: "null",
+      }
+    } else {
+      captchaResponse = {
+        job_mode: job,
+        answers,
+        serverdomain: host,
+        sitekey,
+        motionData: JSON.stringify({
+          st: timestamp,
+          dct: timestamp,
+          mm: getMouseMovements(timestamp),
+        }),
+        n: await getHSL(response.c.req),
+        c: JSON.stringify(response.c)
+      }
+    }
+
+    // Set new headers
+    headers = {
+      "Authority": "hcaptcha.com",
+      "Accept": "application/json",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Content-Type": "application/json",
+      "Origin": "https://assets.hcaptcha.com",
+      "Sec-Fetch-Site": "same-site",
+      "Sec-Fetch-Mode": "cors",
+      "Sec-Fetch-Dest": "empty",
+      "User-Agent": userAgent,
+    };
+
+    // Check answers
+    const checkAnswers = await request(`https://hcaptcha.com/checkcaptcha/${key}`, {
+      method: "post",
+      headers,
+      json: true,
+      body: captchaResponse,
+    });
+
+    if (checkAnswers.generated_pass_UUID) {
+      return checkAnswers.generated_pass_UUID;
+    }
+  } catch (err) {
+    console.log(err);
+    return null;
   };
-  
-  // Check site config
-  let response = await request({
-    method: 'get',
-    headers,
-    json: true,
-    url: `https://hcaptcha.com/checksiteconfig?host=${host}&sitekey=${sitekey}&sc=1&swa=1`
-  });
-
-  let timestamp = Date.now() + rdn(30, 120);
-  
-  // Setup form for getting tasks list 
-  if (response.c === undefined) {
-    form = {
-      sitekey,
-      host,
-      hl: 'en',
-      motionData: {
-        st: timestamp,
-        mm: getMouseMovements(timestamp)
-      },
-    }
-  } else {
-    form = {
-      sitekey,
-      host,
-      hl: 'en',
-      motionData: {
-        st: timestamp,
-        mm: getMouseMovements(timestamp)
-      },
-      n: await getHSL(response.c.req),
-      c: JSON.stringify(response.c)
-    }
-  }
-
-  // Get tasks
-  let getTasks = await request({
-    method: "post",
-    headers,
-    json: true,
-    url: `https://hcaptcha.com/getcaptcha`,
-    form: form
-  });
-
-  if (getTasks.generated_pass_UUID) {
-    return getTasks.generated_pass_UUID;
-  }
-
-  // Find what the captcha is looking for user's to click
-  const requestImageArray = getTasks.requester_question.en.split(" ");
-  let request_image = requestImageArray[requestImageArray.length - 1];
-  if (request_image === "motorbus") {
-    request_image = "bus"
-  } else {
-    request_image = requestImageArray[requestImageArray.length - 1];
-  }
-
-  const key = getTasks.key;
-  if (key.charAt(2) === "_") {
-    return key;
-  }
-
-  const tasks = getTasks.tasklist;
-  const job = getTasks.request_type;
-  timestamp = Date.now() + rdn(30, 120);
-
-  // Get Answers
-  const answers = await getAnswers(request_image, tasks);
-
-  // Renew response
-  response = await request({
-    method: 'get',
-    headers,
-    json: true,
-    url: `https://hcaptcha.com/checksiteconfig?host=${host}&sitekey=${sitekey}&sc=1&swa=1`
-  });
-
-  // Setup data for checking answers
-  if (response.c === undefined) {
-    captchaResponse = {
-      job_mode: job,
-      answers,
-      serverdomain: host,
-      sitekey,
-      motionData: JSON.stringify({
-        st: timestamp,
-        dct: timestamp,
-        mm: getMouseMovements(timestamp),
-      }),
-      n: null,
-      c: "null",
-    }
-  } else {
-    captchaResponse = {
-      job_mode: job,
-      answers,
-      serverdomain: host,
-      sitekey,
-      motionData: JSON.stringify({
-        st: timestamp,
-        dct: timestamp,
-        mm: getMouseMovements(timestamp),
-      }),
-      n: await getHSL(response.c.req),
-      c: JSON.stringify(response.c)
-    }
-  }
-
-  // Set new headers
-  headers = {
-    "Authority": "hcaptcha.com",
-    "Accept": "application/json",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Content-Type": "application/json",
-    "Origin": "https://assets.hcaptcha.com",
-    "Sec-Fetch-Site": "same-site",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Dest": "empty",
-    "User-Agent": userAgent,
-  };
-
-  // Check answers
-  const checkAnswers = await request(`https://hcaptcha.com/checkcaptcha/${key}`, {
-    method: "post",
-    headers,
-    json: true,
-    body: captchaResponse,
-  });
-
-  if (checkAnswers.generated_pass_UUID) {
-    return checkAnswers.generated_pass_UUID;
-  }
 }
 
 async function solveCaptcha(siteKey, host, visionClient) {
